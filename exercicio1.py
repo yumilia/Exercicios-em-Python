d99386e9939f1c136f2911da01e795546e7a032f
Lia Yumi Morimoto <yumi.lia.mori@gmail.com>
2021-02-01 18:37:13 -0300

    Update exercicio1.py

--- exercicio1.py
+++ exercicio1.py
@@ -1,4 +1,5 @@
 # Dados números inteiros n e k, com k >= 0, calcular n elevado a k. Por exemplo, dados os números 3 e 4 o seu programa deve escrever o número 81.
+
 def main():
     n = int(input("Digite o primeiro número: "))
     k = int(input("Digite o segundo número: "))
